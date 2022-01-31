34b550ab460fefbf62cd15dc74aeece16a8e17a4
damianamalraj <damian.amalraj@hotmail.com>
2022-02-01 00:28:57 +0100

    Worked with onClick handler for the card list

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,18 +1,30 @@
 import "../App.scss";
 import "./Home.scss";
 import Card from "../components/Card";
+import { useState } from "react";
 
 const Home = (props) => {
+    let [curent, setCurent] = useState(props.cards[0]);
+
     const card = props.cards.map((card) => {
-        return <Card card={card} key={card.cardNumber} />;
+        const clickHandler = () => {
+            console.log("wow");
+            setCurent(card);
+        };
+        return (
+            <article onClick={clickHandler} key={card.cardNumber}>
+                <Card card={card} />
+            </article>
+        );
     });
+
     return (
         <div className="Home">
             <h1>E-Wallet</h1>
             <p className="active">Active Card</p>
             <Card
-                card={props.cards[0]}
-                key={props.cards[0].cardNumber}
+                card={curent}
+                key={curent.cardNumber}
                 className="active-card"
             />
 
